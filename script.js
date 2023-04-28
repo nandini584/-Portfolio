1dbff5d6b714ea94222c093d163f3f6f303e9512
Nandini Arora <nandini.arora.che22@itbhu.ac.in>
2023-04-29 00:29:41 +0530

    Contact about and resume page ready

---
+++ script.js
@@ -0,0 +1,24 @@
+document.getElementsByClassName("fa")[0].addEventListener("mouseover", function(e) {
+    e.target.classList.add("fa-spin");
+})
+document.getElementsByClassName("fa")[0].addEventListener("mouseout", function(e) {
+    e.target.classList.remove("fa-spin");
+})
+document.getElementsByClassName("fa")[1].addEventListener("mouseover", function(e) {
+    e.target.classList.add("fa-spin");
+})
+document.getElementsByClassName("fa")[1].addEventListener("mouseout", function(e) {
+    e.target.classList.remove("fa-spin");
+})
+document.getElementsByClassName("fa")[2].addEventListener("mouseover", function(e) {
+    e.target.classList.add("fa-spin");
+})
+document.getElementsByClassName("fa")[2].addEventListener("mouseout", function(e) {
+    e.target.classList.remove("fa-spin");
+})
+document.getElementsByClassName("fa")[3].addEventListener("mouseover", function(e) {
+    e.target.classList.add("fa-spin");
+})
+document.getElementsByClassName("fa")[3].addEventListener("mouseout", function(e) {
+    e.target.classList.remove("fa-spin");
+})
